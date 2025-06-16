ca51306ca7fb97a935ce59fcce0ddbbe7e39064d
AndreaMargiotta2007 <84795731+AndreaMargiotta2007@users.noreply.github.com>
2025-06-16 19:40:17 +0000

    modificato3

--- Scrittura.php
+++ Scrittura.php
@@ -1,6 +1,16 @@
 <?php
 include "Connection.php";
-$nome = 
+if($_SERVER["REQUEST_METHOD"] == 'POST'){
+    $nome = $_POST['nome'];
+
+    $stmt = $conn->prepare("INSERT INTO ipad(nome) VALUES(?)");
+    $stmt->bind_param("s", $nome);
+    $stmt->execute();
+
+    $stmt->close();
+    
+}
+
 
 
 ?>
