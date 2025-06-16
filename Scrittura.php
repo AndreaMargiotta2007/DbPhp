df1f087bcf875bd061bf0bce8ac97c5df155b5a1
AndreaMargiotta2007 <84795731+AndreaMargiotta2007@users.noreply.github.com>
2025-06-16 19:45:49 +0000

    modificato3

--- Scrittura.php
+++ Scrittura.php
@@ -2,13 +2,23 @@
 include "Connection.php";
 if($_SERVER["REQUEST_METHOD"] == 'POST'){
     $nome = $_POST['nome'];
+    $conn->begin_transaction();
+    try{
+        $stmt = $conn->prepare("INSERT INTO ipad(nome) VALUES(?)");
+        $stmt->bind_param("s", $nome);
+        $stmt->execute();
 
-    $stmt = $conn->prepare("INSERT INTO ipad(nome) VALUES(?)");
-    $stmt->bind_param("s", $nome);
-    $stmt->execute();
+        $stmt->commit();
+
+        echo "Riuscito";
+
+    } catch(Exception $e){
+        $conn->rollback();
+        echo "errore";
+    }
+    
 
     $stmt->close();
-    
 }
 
 
