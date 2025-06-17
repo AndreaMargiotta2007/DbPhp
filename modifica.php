2316bf8fdc3be91601e5653b5d9b77e7047205e7
AndreaMargiotta2007 <84795731+AndreaMargiotta2007@users.noreply.github.com>
2025-06-17 08:16:57 +0000

    namo

---
+++ modifica.php
@@ -0,0 +1,22 @@
+<?php
+include "Connection.php";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $conn->begin_transaction();
+    try{
+        $nome = $_POST['nome'];
+        $id = $_POST['id'];
+        $stmt = $conn->prepare("ALTER TABLE ipad set nome = ? where id = ?");
+        $stmt->bind_param('si', $nome, $id);
+
+        $stmt->execute();
+
+        $conn->commit();
+        echo "fatto";
+    }catch(Exception $e){
+        $conn->rollback();
+        echo "errore";
+    }
+}
+
+?>
