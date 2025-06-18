2cc415c9932e5dc119f4acfa653ba1c3f0702f58
AndreaMargiotta2007 <84795731+AndreaMargiotta2007@users.noreply.github.com>
2025-06-18 19:59:00 +0000

    modificato

--- login.php
+++ login.php
@@ -11,15 +11,26 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         $stmt = $conn->prepare("SELECT pass FROM utentiipad where email = ?");
         $stmt->bind_param("s", $email);
         $stmt->execute();
-        $stmt->bind_result($dbPassword);
+        $stmt->store_result();
+
+        if($stmt->num_rows == 0){
+            echo "Email non trovata.";
+            $conn->rollback();
+        } else {
+            $stmt->bind_result($dbPassword);
+
         $stmt->fetch();
+        }
+        
 
         if(password_verify($pass, $dbPassword)){
             echo "Accesso riuscito";
+            $conn->commit();
         } else {
             echo "Email o password errati.";
+            $conn->rollback();
         }
-        $conn->commit();
+        
     }catch(Exception $e){
         die("<br>Errore");
     }
